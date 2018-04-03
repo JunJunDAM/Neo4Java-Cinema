b83293e6885a97c726f2b10ac349b1c48e030cb5
Jun <juninfo1997@gmail.com>
2018-04-03 17:52:33 +0200

    switch finished

--- src/neo4javaproject/Neo4JavaProject.java
+++ src/neo4javaproject/Neo4JavaProject.java
@@ -25,14 +25,134 @@ public class Neo4JavaProject {
      */
     public static void main(String[] args) {
         GraphDatabaseFactory graphDBFactory = new GraphDatabaseFactory();
-        GraphDatabaseService graphDBService = graphDBFactory.newEmbeddedDatabase(new File("C:/Neo4jJavaProject"));
+        GraphDatabaseService graphDBService = graphDBFactory.newEmbeddedDatabase(new File("C:/Neo4jJavaProjectDataBase"));
+        int menuOption;
 
-        
-        int menuOption = pedirEntero();
-        switch(menuOption){
-            
-        }
-        
+        do {
+            menu();
+            menuOption = pedirEntero();
+            switch (menuOption) {
+                case 1: //New user
+                    try (Transaction tx = graphDBService.beginTx()) {
+                        String username = pedirCadena("Introduce nombre : ");
+                        Node user1 = graphDBService.createNode(Labels.USERS);
+                        user1.setProperty("name", username);
+                        tx.success();
+                    }
+                    break;
+                case 2: //New film
+                    try (Transaction tx = graphDBService.beginTx()) {
+                        String filmName = pedirCadena("Introduce un titulo: ");
+                        Node film1 = graphDBService.createNode(Labels.MOVIES);
+                        film1.setProperty("title", filmName);
+                        tx.success();
+                    }
+                    break;
+                case 3: //See film
+                    try (Transaction tx = graphDBService.beginTx()) {
+                        ResourceIterator<Node> movies = graphDBService.findNodes(Labels.MOVIES);
+                        ResourceIterator<Node> users = graphDBService.findNodes(Labels.USERS);
+
+                        String username = pedirCadena("Introduce nombre de usuario : ");
+                        String filmName = pedirCadena("Introduce titulo de pelicula : ");
+                        System.out.println("Que nota le pone " + username + " a " + filmName + " ? : ");
+                        int nota = pedirEntero();
+
+                        Node user = null;
+                        Node movie = null;
+
+                        while (users.hasNext()) {
+                            Node user2 = users.next();
+                            if (username.equalsIgnoreCase(String.valueOf(user2.getProperty("name")))) {
+                                user = user2;
+                            }
+                        }
+
+                        while (movies.hasNext()) {
+                            Node movie2 = movies.next();
+                            if (filmName.equalsIgnoreCase(String.valueOf(movie2.getProperty("title")))) {
+                                movie = movie2;
+                            }
+                        }
+
+                        System.out.println("*** " + user.getProperty("name") + " ha visto " + movie.getProperty("title")
+                                + " y le ha puesto un " + nota + " de nota");
+                        seeMovie(user, movie, nota);
+                        tx.success();
+                    }
+                    break;
+                case 4: //Notas de peliculas
+                    try (Transaction tx = graphDBService.beginTx()) {
+                        ResourceIterator<Node> movies = graphDBService.findNodes(Labels.MOVIES);
+                        movies = graphDBService.findNodes(Labels.MOVIES);
+                        System.out.println("");
+                        System.out.println("Movies ratings : ");
+                        while (movies.hasNext()) {
+                            Node movie = movies.next();
+                            Iterable<Relationship> relationships = movie.getRelationships(
+                                    Direction.INCOMING, RelationShipTypes.HAS_SEEN);
+                            int totalStars = 0;
+                            int relationshipCount = 0;
+                            for (Relationship relationship : relationships) {
+                                int stars = (int) relationship.getProperty("stars");
+                                totalStars += stars;
+                                relationshipCount++;
+                            }
+                            System.out.println(movie.getProperty("title") + ", Viewers: "
+                                    + relationshipCount + ", Average rating: "
+                                    + totalStars / relationshipCount);
+                        }
+                        tx.success();
+                    }
+                    break;
+                case 5: //Peliculas vistas por usuarios
+                    try (Transaction tx = graphDBService.beginTx()) {
+                        ResourceIterator<Node> users = graphDBService.findNodes(Labels.USERS);
+                        users = graphDBService.findNodes(Labels.USERS);
+                        while (users.hasNext()) {
+                            Node user = users.next();
+                            System.out.println(user.getProperty("name")
+                                    + " has seen :");
+                            for (Relationship relationship : user.getRelationships(RelationShipTypes.HAS_SEEN)) {
+                                Node movie = relationship.getOtherNode(user);
+                                System.out.println(movie.getProperty("title"));
+                            }
+                            System.out.println("");
+                        }
+                        tx.success();
+                    }
+                    break;
+                case 6:
+                    System.out.println("Aplicacion cerrada");
+                    break;
+                case 7:
+                    try (Transaction tx = graphDBService.beginTx()) {
+                        //Muesto las peliculas
+                        ResourceIterator<Node> movies = graphDBService.findNodes(Labels.MOVIES);
+                        System.out.println("Movies: ");
+                        while (movies.hasNext()) {
+                            Node movie = movies.next();
+                            System.out.print(movie.getProperty("title") + " ; ");
+                        }
+
+                        //Muestro los usuarios
+                        ResourceIterator<Node> users = graphDBService.findNodes(Labels.USERS);
+                        System.out.println("");
+                        System.out.println("Users: ");
+                        while (users.hasNext()) {
+                            Node user = users.next();
+                            System.out.print(user.getProperty("name") + " ; ");
+                        }
+                        System.out.println("");
+                        tx.success();
+                    }
+                    break;
+                default:
+                    System.err.println("Opcion incorrecta");
+            }
+        } while (menuOption != 6);
+
+        /*
         try (Transaction tx = graphDBService.beginTx()) {
 
             //Creo usuarios
@@ -111,6 +231,7 @@ public class Neo4JavaProject {
 
             tx.success();
         }
+         */
     }
 
     //Creo la relacion HAS_SEEN para tener una relacion entre usuarios y peliculas
@@ -128,7 +249,7 @@ public class Neo4JavaProject {
         System.out.println("********** [2] - New Film             **********");
         System.out.println("********** [3] - Show film            **********");
         System.out.println("********** [4] - Films average        **********");
-        System.out.println("********** [5] -  **********");
+        System.out.println("********** [5] - Users showed films    *********");
         System.out.println("********** [6] - Exit                 **********");
         System.out.println("************************************************");
         System.out.println("********** Escoge una opcion: ");
@@ -146,27 +267,6 @@ public class Neo4JavaProject {
                 error = false;
             } catch (IOException ex) {
                 System.out.println("Error de entrada/salida");
-                error = true;
-            }
-        } while (error);
-        return respuesta;
-    }
-
-    //Funcion para pedir doubles
-    private static double pedirDouble(String mensaje) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        double respuesta = 0;
-        boolean error;
-        do {
-            try {
-                System.out.print(mensaje);
-                respuesta = Double.parseDouble(br.readLine());
-                error = false;
-            } catch (IOException ex) {
-                System.out.println("Error de entrada/salida");
-                error = true;
-            } catch (NumberFormatException ex) {
-                System.out.println("No has introducido un numero decimal");
                 error = true;
             }
         } while (error);
