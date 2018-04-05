d56d674a41bd7f0e925f419d4a261825fd7137cc
Jun <juninfo1997@gmail.com>
2018-04-05 18:23:24 +0200

    Aplication finished

--- src/neo4javaproject/Neo4JavaProject.java
+++ src/neo4javaproject/Neo4JavaProject.java
@@ -8,11 +8,15 @@ package neo4javaproject;
 import java.io.*;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.traversal.Evaluators;
+import org.neo4j.graphdb.traversal.TraversalDescription;
+import org.neo4j.graphdb.traversal.Traverser;
 
 /**
  *
@@ -28,6 +32,29 @@ public class Neo4JavaProject {
         GraphDatabaseService graphDBService = graphDBFactory.newEmbeddedDatabase(new File("C:/Neo4jJavaProjectDataBase"));
         int menuOption;
 
+        try (Transaction tx = graphDBService.beginTx()) {
+            Node user1 = graphDBService.createNode(Labels.USERS);
+            Node user2 = graphDBService.createNode(Labels.USERS);
+            Node user3 = graphDBService.createNode(Labels.USERS);
+            user1.setProperty("name", "user1");
+            user2.setProperty("name", "user2");
+            user3.setProperty("name", "user3");
+            user1.createRelationshipTo(user2, RelationShipTypes.IS_FRIEND_OF);
+            
+            Node film1 = graphDBService.createNode(Labels.MOVIES);
+            Node film2 = graphDBService.createNode(Labels.MOVIES);
+            Node film3 = graphDBService.createNode(Labels.MOVIES);
+            film1.setProperty("title", "film1");
+            film2.setProperty("title", "film2");
+            film3.setProperty("title", "film3");
+            
+            seeMovie(user1, film1, 5);
+            seeMovie(user2, film2, 5);
+            seeMovie(user1, film2, 2);
+            seeMovie(user3, film3, 10);
+            tx.success();
+        }
+        
         do {
             menu();
             menuOption = pedirEntero();
@@ -122,64 +149,58 @@ public class Neo4JavaProject {
                         tx.success();
                     }
                     break;
-                case 6:
-                    System.out.println("Aplicacion cerrada");
-                    break;
-                case 7:
+                case 6: //Hago amigos
                     try (Transaction tx = graphDBService.beginTx()) {
-                        //Muesto las peliculas
-                        ResourceIterator<Node> movies = graphDBService.findNodes(Labels.MOVIES);
-                        System.out.println("Movies: ");
-                        while (movies.hasNext()) {
-                            Node movie = movies.next();
-                            System.out.print(movie.getProperty("title") + " ; ");
-                        }
-
-                        //Muestro los usuarios
                         ResourceIterator<Node> users = graphDBService.findNodes(Labels.USERS);
-                        System.out.println("");
-                        System.out.println("Users: ");
+                        
+                        String username1 = pedirCadena("Introduce nombre de usuario : ");
+                        String username2 = pedirCadena("Introduce nombre de usuario2 : ");
+                        
+                        Node user1= null;
+                        Node user2 = null;
+
                         while (users.hasNext()) {
-                            Node user = users.next();
-                            System.out.print(user.getProperty("name") + " ; ");
+                            Node user11 = users.next();
+                            if (username1.equalsIgnoreCase(String.valueOf(user11.getProperty("name")))) {
+                                user1 = user11;
+                            }
+                            if(username2.equalsIgnoreCase(String.valueOf(user11.getProperty("name")))){
+                                user2 = user11;
+                            }
                         }
-                        System.out.println("");
+                        
+                        user1.createRelationshipTo(user2, RelationShipTypes.IS_FRIEND_OF);
+                        
                         tx.success();
                     }
+                    break;
+                case 7: //Amigos de una persona
+                    try (Transaction tx = graphDBService.beginTx()) {
+                        String username = pedirCadena("Introduce nombre de usuario : ");
+                        Node user = graphDBService.findNode(Labels.USERS, "name", username);
+                        TraversalDescription myFriends = graphDBService.traversalDescription()
+                                .breadthFirst()
+                                .relationships(RelationShipTypes.IS_FRIEND_OF)
+                                .evaluator(Evaluators.atDepth(1));
+                        Traverser traverser = myFriends.traverse(user);
+                        
+                        System.out.println("Amigos de " + username + " : ");
+                        for(Node friend : traverser.nodes()){
+                            System.out.println(friend.getProperty("name"));
+                        }
+                        tx.success();
+                    }
+                    break;
+                case 8:
+                    System.out.println("Aplicacion cerrada");
                     break;
                 default:
                     System.err.println("Opcion incorrecta");
             }
-        } while (menuOption != 6);
+        } while (menuOption != 8);
 
         /*
         try (Transaction tx = graphDBService.beginTx()) {
-
-            //Creo usuarios
-            Node usu1 = graphDBService.createNode(Labels.USERS);
-            usu1.setProperty("name", "Sergio");
-            Node usu2 = graphDBService.createNode(Labels.USERS);
-            usu2.setProperty("name", "Jun");
-            Node usu3 = graphDBService.createNode(Labels.USERS);
-            usu3.setProperty("name", "Dani");
-
-            //Creo peliculas
-            Node movie1 = graphDBService.createNode(Labels.MOVIES);
-            movie1.setProperty("name", "Torrente");
-            Node movie2 = graphDBService.createNode(Labels.MOVIES);
-            movie2.setProperty("name", "Titanic");
-            Node movie3 = graphDBService.createNode(Labels.MOVIES);
-            movie3.setProperty("name", "King Kong");
-
-            //Hago relaciones IS_FRIEND_OF entre usu1 y el resto
-            usu1.createRelationshipTo(usu2, RelationShipTypes.IS_FRIEND_OF);
-            usu1.createRelationshipTo(usu3, RelationShipTypes.IS_FRIEND_OF);
-
-            //Creo una relacion tipo HAS_SEEN entre los usuarios y las peliculas
-            seeMovie(usu1, movie1, 10);
-            seeMovie(usu2, movie1, 6);
-            seeMovie(usu3, movie2, 5);
-
             //Muesto las peliculas
             ResourceIterator<Node> movies = graphDBService.findNodes(Labels.MOVIES);
             System.out.println("Movies: ");
@@ -196,39 +217,6 @@ public class Neo4JavaProject {
                 Node user = users.next();
                 System.out.print(user.getProperty("name") + " ; ");
             }
-
-            //Notas que tienen peliculas
-            movies = graphDBService.findNodes(Labels.MOVIES);
-            System.out.println("");
-            System.out.println("Movies ratings : ");
-            while (movies.hasNext()) {
-                Node movie = movies.next();
-                Iterable<Relationship> relationships = movie.getRelationships(
-                        Direction.INCOMING, RelationShipTypes.HAS_SEEN);
-                int totalStars = 0;
-                int relationshipCount = 0;
-                for (Relationship relationship : relationships) {
-                    int stars = (int) relationship.getProperty("stars");
-                    totalStars += stars;
-                    relationshipCount++;
-                }
-                System.out.println(movie.getProperty("name") + ", Viewers: "
-                        + relationshipCount + ", Average rating: "
-                        + totalStars / relationshipCount);
-            }
-            //Peliculas vistas por usuarios
-            users = graphDBService.findNodes(Labels.USERS);
-            while (users.hasNext()) {
-                Node user = users.next();
-                System.out.print(user.getProperty("name")
-                        + " has seen :");
-                for (Relationship relationship : user.getRelationships(RelationShipTypes.HAS_SEEN)) {
-                    Node movie = relationship.getOtherNode(user);
-                    System.out.println(movie.getProperty("name"));
-                }
-                System.out.println("");
-            }
-
             tx.success();
         }
          */
@@ -249,8 +237,10 @@ public class Neo4JavaProject {
         System.out.println("********** [2] - New Film             **********");
         System.out.println("********** [3] - Show film            **********");
         System.out.println("********** [4] - Films average        **********");
-        System.out.println("********** [5] - Users showed films    *********");
-        System.out.println("********** [6] - Exit                 **********");
+        System.out.println("********** [5] - Users showed films   **********");
+        System.out.println("********** [6] - Make friends         **********");
+        System.out.println("********** [7] - My friends           **********");
+        System.out.println("********** [8] - Exit                  **********");
         System.out.println("************************************************");
         System.out.println("********** Escoge una opcion: ");
     }
